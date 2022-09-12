43388fee484269e32643b4da649f95b1d06612b8
Piyush Kumar <2820058.cse@pietgroup.co.in>
2022-09-12 22:02:56 +0530

    first commit

---
+++ capp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name ="index"),
+    path('about/', about, name = "about"),
+    path('index_1/', index_1, name = "index_1"),
+    path('contact/', contact, name = "contact"),
+    #path('main/', main, name = "main"),
+    path('blog/', blog, name = "blog"),
+    path('prevention/', prevention, name = "prevention"),
+    path('single/', single, name = "single"),
+    path('symptoms/', symptoms, name = "symptoms"),
+]
---
+++ capp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import datetime
+from django import template
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def index_1(request):
+    return render(request, 'index_1.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {}) 
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+#def main(request):
+   # return render(request, 'main.html', {})
+
+def prevention(request):
+    return render(request, 'prevention.html', {})
+
+def single(request):
+    return render(request, 'single.html', {})
+
+def symptoms(request):
+    return render(request, 'symptoms.html', {})
+
